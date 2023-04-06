24cb7794796d906d76ed6a0dd14261bea3abeadc
Mystol <marcnouwligbeto@gmail.com>
2023-04-06 11:21:12 +0100

    add js

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,16 @@
-const button = document.querySelector('button')
-button.addEventListener('click', () => {
-    alert('Bonjour')
+let theme = document.querySelector('.theme')
+let body = document.querySelector('body')
+let first = document.querySelectorAll('.dash nav ul li')
+
+theme.addEventListener('click', function () {
+	theme.classList.toggle('clicked')
+    body.classList.toggle('dark-theme')
+})
+
+first.forEach(element => {
+	element.addEventListener('click', function () {
+		// element.classList.toggle('active')
+		element.classList.remove('active')
+		element.classList.add('active')
+	})
 })
