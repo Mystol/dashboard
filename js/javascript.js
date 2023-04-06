86fdb5f91882e0ca91fa01c1c0d49df104997d3f
Mystol <marcnouwligbeto@gmail.com>
2023-04-06 14:39:03 +0100

    update loading

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,21 @@
 let theme = document.querySelector('.theme')
 let body = document.querySelector('body')
 let first = document.querySelectorAll('.dash nav ul li')
+let span = document.querySelector('.dash nav ul span')
 
 theme.addEventListener('click', function () {
 	theme.classList.toggle('clicked')
     body.classList.toggle('dark-theme')
 })
 
-first.forEach(element => {
+first.forEach((element, k) => {
 	element.addEventListener('click', function () {
-		// element.classList.toggle('active')
-		element.classList.remove('active')
+		span.style.transform = `translateY(${k*3.8}em)`
+		
+		let parent = element.parentNode
+		let btn = parent.querySelector('.active')
+		btn.classList.remove('active')
+
 		element.classList.add('active')
 	})
 })
